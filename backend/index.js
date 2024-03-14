039b5caf89b539554f0985b921a0360dd8810368
Julien Kohler <jujukoko@pop-os.localdomain>
2024-03-14 15:10:55 +0100

    first_versio_quot_get_backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/quote',async (req, res) => {
+  try {
+    //get request to quotable API
+    const response = await axios.get('https://api.quotable.io/quotes/random'); //await because axios return Promise
+    
+    //extraction the citation from de response
+    const { content, author } = response.data[0]; //response.data is an array need to take the first elem
+    //return response in JSON format
+    res.json({ content, author });
+  } catch (error) {
+    //Throw error to frontend if error during the process
+    res.status(500).json({ error: 'Une erreur s\'est produite lors de la récupération de la citation' });
+  }
 });
 
+//start serveur
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
